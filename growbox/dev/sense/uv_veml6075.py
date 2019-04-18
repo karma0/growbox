2475269aaa77a0782c8f836bd2dbe47410dd3881
Bobby <rlarson@lgsinnovations.com>
2019-04-18 17:11:48 -0600

    Starting on the UV sensor

--- growbox/dev/sense/uv_veml6075.py
+++ growbox/dev/sense/uv_veml6075.py
@@ -1,65 +1,76 @@
 # -*- coding: utf-8 -*-
 
-"""Relay implementations"""
+"""UV sensor implementations for the VEML6075 chip."""
 
 
 from growbox.wire import Wire
 
+from enum import Enum
 
-class QuadRelay(Wire):
+
+class Resolution(Enum):
+    NORMAL = 0
+    HIGH = 1
+    INVALID = 2
+
+
+class UVIntegration(Enum):
+    IT_50MS = 50
+    IT_100MS = 100
+    IT_200MS = 200
+    IT_400MS = 400
+    IT_800MS = 800
+
+
+class VEML6075Error(Enum):
+    ERROR_READ = -4
+    ERROR_WRITE = -3
+    ERROR_INVALID_ADDRESS = -2
+    ERROR_UNDEFINED = -1
+    ERROR_SUCCESS = 1
+
+
+class Registers(Enum):
+    UV_CONF = 0x00
+    UVA_DATA = 0x07
+    UVB_DATA = 0x09
+    UVCOMP1_DATA = 0x0A
+    UVCOMP2_DATA = 0x0B
+    ID = 0x0C
+
+
+class UVSensor(Wire):
     """
-    SparkFun's QWIIC quad relay device and corresponding interface.
+    SparkFun's QWIIC VEML6075 UV sensor and corresponding interface.
     """
-    address = 0x6D
-    jumper_address = 0x6C
+    address = 0x10
 
-    status_offset = 0x05
+    status = VEML6075Error.ERROR_UNDEFINED
 
-    # commands
-    turn_all_off = 0xA
-    turn_all_on = 0xB
-    toggle_all = 0xC
+    uv_integration = UVIntegration.IT_100MS
 
-    status_results = {
-        0: False,
-        15: True,
-    }
+    raw_uva = 0
+    raw_uvb = 0
+    raw_dark = 0
+    raw_ir = 0
+    raw_viz = 0
 
-    relay_addresses = [
-        0x01,
-        0x02,
-        0x03,
-        0x04,
-    ]
+    config
 
-    def all_off(self):
-        """Turn off all relays."""
-        self.write(self.turn_all_off)
+    def __init__(self, *args, samplerate=None, **kwargs):
+        if samplerate is not None:
+            if not isinstance(samplerate, UVIntegration):
+                raise ValueError("Invalid UVIntegration as a samplerate: "
+                                 f"{samplerate}")
+            self.uv_integration = samplerate
+            self.set_integration_time()
 
-    def all_on(self):
-        """Turn on all relays."""
-        self.write(self.turn_all_on)
+        super().__init__(*args, **kwargs)
+        self.config |= 
 
-    def all_toggle(self):
-        """Toggle all relays."""
-        self.write(self.toggle_all)
+    def set_integration_time(self, samplerate=None):
+        if samplerate is not None:
+            self.uv_integration = samplerate
 
-    def toggle(self, relay_id):
-        """Toggle a relay by index starting at 0."""
-        self.write(self.relay_addresses[relay_id])
+        self.read(Registers.UV_CONF)
 
-    def status(self, relay_id):
-        """Check a relay status by index starting at 0."""
-        return self.status_results[
-            self.read(self.status_offset + relay_id)]
-
-    def all_status(self):
-        """Check all relays status."""
-        return [
-            self.status_results[resp] for resp in
-            self.read(self.status_offset, len(self.relay_addresses))
-        ]
-
-
-class Relay:
-    pass
