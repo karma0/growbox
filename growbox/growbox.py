4f24e264e5118e681aba571a5f8f0a166488d2d2
Bobby <karmma0@gmail.com>
2019-05-18 18:21:02 -0600

    Removed UV sensor

--- growbox/growbox.py
+++ growbox/growbox.py
@@ -16,7 +16,7 @@ from growbox.dev.actuate.relay import QuadRelay
 from growbox.dev.sense.ds18b20 import DS18B20
 from growbox.dev.sense.bme280 import BME280
 from growbox.dev.sense.ccs811 import CCS811
-from growbox.dev.sense.veml6075 import VEML6075
+#from growbox.dev.sense.veml6075 import VEML6075
 from growbox.dev.sense.tsl2591 import TSL2591
 
 from growbox.facade.relay import Relay
@@ -48,7 +48,7 @@ class GrowBox:
         self.bme280 = BME280()
         self.ccs811 = CCS811()
         self.lux = TSL2591()
-        self.veml = VEML6075()
+        #self.veml = VEML6075()
         self.quad_relay = QuadRelay()
         self.mister = Relay(self.quad_relay, relay_id=self.relays.get('mister'))
         self.fans = Fans()
@@ -64,9 +64,9 @@ class GrowBox:
             ('co2', self.ccs811),
             ('tvoc', self.ccs811),
             ('lux', self.lux),
-            ('uv_index', self.veml),
-            ('uva', self.veml),
-            ('uvb', self.veml),
+            #('uv_index', self.veml),
+            #('uva', self.veml),
+            #('uvb', self.veml),
             ('mister_status', self),
             ('upper_fans_status', self.fans),
             ('lower_fans_status', self.fans),
