2f2f55fe8b66cb36c0e92949cc538d49f6a203f7
Bobby <karmma0@gmail.com>
2019-05-19 08:49:51 -0600

    Swapped upper and lower fan pins

--- growbox/dev/actuate/fans.py
+++ growbox/dev/actuate/fans.py
@@ -30,8 +30,8 @@ class Motors(Enum):
 class Fans:
     address = 0x40
 
-    upper_fan_pins = (Motors.MOTOR1, Motors.MOTOR2)
-    lower_fan_pins = (Motors.MOTOR3, Motors.MOTOR4)
+    upper_fan_pins = (Motors.MOTOR3, Motors.MOTOR4)
+    lower_fan_pins = (Motors.MOTOR1, Motors.MOTOR2)
 
     upper_fans = []
     lower_fans = []
