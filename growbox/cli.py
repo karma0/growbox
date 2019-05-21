97033600989611f08a958d5e98e8bd5354be60a6
Bobby <karmma0@gmail.com>
2019-05-20 19:12:32 -0600

    Added exception handling and restart

--- growbox/cli.py
+++ growbox/cli.py
@@ -4,14 +4,23 @@
 import sys
 import click
 
+import logging
+
 from growbox.growbox import GrowBox
+
+
+logger = logging.getLogger(__name__)
 
 
 @click.command()
 def main(args=None):
     """Console script for growbox."""
-    box = GrowBox()
-    box.run()
+    while True:
+        try:
+            box = GrowBox()
+            box.run()
+        except Exception as ex:
+            logger.error(str(ex))
     return 0
 
 
